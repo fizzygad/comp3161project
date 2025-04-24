9039c40815a4c94ad1c60d64b67bb67fdd4a94da
Joel Foster <janthonyf70@gmail.com>
2025-04-23 21:53:06 -0500

    changes for railway

--- DBMSProjectAPI.py
+++ DBMSProjectAPI.py
@@ -369,4 +369,4 @@ def create_forum():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=int(os.getenv("PORT", 5000)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.getenv("PORT", 5000)))
